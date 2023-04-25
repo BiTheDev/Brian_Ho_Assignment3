ee351d29b317adb2d2975dc0839547931f6fc6a0
Brian Ho <brian@MacBook-Pro.local>
2023-04-24 20:32:51 -0700

    homepage WIP

--- social-app-client/src/pages/HomePage.jsx
+++ social-app-client/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { useAuth } from "../authContext";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
+  const { user } = useAuth();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,20 +20,30 @@ const HomePage = () => {
     fetchPosts();
   }, []);
 
+  // Implement your create, edit, and delete post logic here
+
   return (
     <div>
       <h1>Recent Posts</h1>
       {posts.length > 0 ? (
         posts.map((post) => (
           <div key={post._id}>
-            <h3>{post.username}</h3>
+            <h3>
+              <Link to={`/users/${post.username}`}>{post.username}</Link>
+            </h3>
             <p>{post.content}</p>
             <p>{new Date(post.timestamp).toLocaleString()}</p>
+            {user && user.username === post.username && (
+              <div>
+                {/* Provide options to edit and delete the post */}
+              </div>
+            )}
           </div>
         ))
       ) : (
         <p>Loading posts...</p>
       )}
+      {/* Add a form to create a new post */}
     </div>
   );
 };
